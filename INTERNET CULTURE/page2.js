d0eeee4e45bf6315f7a5eac6fbcbcfa657982179
Joy Jimmy <68251425+jjimmy0@users.noreply.github.com>
2025-05-13 02:41:01 +0100

    Delete INTERNET CULTURE directory

--- INTERNET CULTURE/page2.js
+++
@@ -1,9 +0,0 @@
-const words = document.querySelectorAll('.word');
-
-words.forEach((word, index) => {
-  const delay = (Math.random() * 4).toFixed(2); // 0–4 seconds
-  const duration = (3 + Math.random() * 3).toFixed(2); // 3–6 seconds
-
-  word.style.animationDelay = `${delay}s`;
-  word.style.animationDuration = `${duration}s`;
-});
